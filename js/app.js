45867a48d5fcdbc8fd74d906e44bbbb312d908c0
dragons680114-star <dragons680114@gmail.com>
2026-08-07 10:21:10 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { initLoading } from "./components/loading.js";
 import { initPageTransition } from "./components/page-transition.js";
 import { initProgressNavigation } from "./components/progress-navigation.js";
-import { initSectionComponents } from "./components/sections/index.js?v=20260807-fullscreenfix";
+import { initSectionComponents } from "./components/sections/index.js?v=20260807-finalinteract";
 import { initTheme } from "./components/theme.js";
 import { publishDependencyStatus } from "./core/dependencies.js";
 import { initScrollEffects } from "./features/scroll-effects.js";
